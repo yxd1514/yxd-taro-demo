83a9e5a31b1bd07555a4aec25b1749e3cc5ac703
yxd1514 <936331852@qq.com>
2023-07-24 09:39:03 +0800

    1

--- src/pages/detail/detail.vue
+++ src/pages/detail/detail.vue
@@ -19,6 +19,11 @@ import './detail.scss'
 import { ref } from 'vue'
 import { getCurrentInstance } from '@tarojs/taro';
 import Taro from '@tarojs/taro'
+import image1 from '../../../static/images/image1.jpg'
+import image2 from '../../../static/images/image2.jpg'
+import image3 from '../../../static/images/image3.jpg'
+import image4 from '../../../static/images/image4.jpg'
+import image5 from '../../../static/images/image5.jpg'
 console.log('setup run.');
 
 const {router}  = getCurrentInstance()
@@ -27,31 +32,31 @@ const details = ref([
   {
     id: 1,
     title: '我们要面对的苦难和挫折不会因为我们遭遇够多而减少',
-    image: require('../../static/images/image1.jpg'),
+    image: image1,
     contain: '我们要面对的苦难和挫折不会因为我们遭遇够多而减少，上天不会因为你的悲惨而悯怜你，它只会安排一道又一道难关来磨炼你的意志。'
   },
   {
     id: 2,
     title: '你可以靠透支身体',
-    image: require('../../static/images/image2.jpg'),
+    image: image2,
     contain: '在某个年纪之前，你可以靠透支身体，小聪明和老天给你的运气一直取巧地活着。然而到了某个年纪之后，真正能让你走远的，都是自律，积极和勤奋'
   },
   {
     id: 3,
     title: '我们最终都要远行',
-    image: require('../../static/images/image3.jpg'),
+    image: image3,
     contain: '我们最终都要远行，最终都要跟稚嫩的自己告别。也许路途有点艰辛，有点孤独，但熬过了痛苦，我们才能得以成长。'
   },
   {
     id: 4,
     title: '这就是所谓的人生',
-    image: require('../../static/images/image4.jpg'),
+    image: image4,
     contain: '所有回不去的良辰美景，都是举世无双的好时光。感谢过去，珍惜现在，憧憬未来。哭给自己听，笑给别人看，这就是所谓的人生。'
   },
   {
     id: 5,
     title: '去做最好的自己',
-    image: require('../../static/images/image5.jpg'),
+    image: image5,
     contain: '别人再好，也是别人。自己再不堪，也是自己，独一无二的自己。只要努力去做最好的自己，一生足矣。为自己的人生负责，为自己的梦想买单。'
   },
 ])
